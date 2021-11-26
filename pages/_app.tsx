930a8ba6834d8aa5246f7bb875f8e9fbc22b5325
anthony <quereantho@gmail.com>
2021-11-26 18:54:10 +0100

    :lipstick: Add button component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import {Toaster} from "react-hot-toast";
+
 
 function MyApp({Component, pageProps}: AppProps) {
   return <>
