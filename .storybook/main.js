df2572e746d08e1f1271b2ea5f8adf7bc7102739
anthony <quereantho@gmail.com>
2021-11-26 21:30:52 +0100

    :heavy_plus_sign: Add and configure Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "stories": [
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+        cssLoaderOptions: {
+          modules: {
+            auto: true,
+          },
+          importLoaders: 3,
+        },
+      },
+    },
+  ],
+  "framework": "@storybook/react",
+}
