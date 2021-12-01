e9221573ff2f7666325a63716c2babf422c77123
anthony <quereantho@gmail.com>
2021-12-01 11:38:05 +0100

    :wheelchair: Improve Modal accessibility

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,26 @@
 module.exports = {
-  "stories": [
-    "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
+    "stories": [
+        "../components/**/*.stories.mdx",
+        "../components/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-a11y",
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss'),
+                },
+                cssLoaderOptions: {
+                    modules: {
+                        auto: true,
+                    },
+                    importLoaders: 3,
+                },
+            },
         },
-        cssLoaderOptions: {
-          modules: {
-            auto: true,
-          },
-          importLoaders: 3,
-        },
-      },
-    },
-  ],
-  "framework": "@storybook/react",
+    ],
+    "framework": "@storybook/react",
 }
